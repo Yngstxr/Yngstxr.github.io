7d3eb8df94e8bfcc4a3e8217b033451c71e382f5
Yngstxr <lomakindima168@gmail.com>
2026-09-21 09:21:36 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,56 @@
-const CACHE_NAME = 'physcalc-v1.0.2';
+const CACHE_NAME = 'physcalc-v1.0.3';
 const ASSETS = [
   '/',
   '/index.html',
   '/prjamieizmer.html',
   '/manifest.json',
   '/index3.html',
-  '/icon-192.png',
   '/icon-128.png',
+  '/icon-192.png',
   '/icon-256.png',
   '/icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse; 
-      }
-      return fetch(e.request).catch(() => {
-        console.log('Запрос отклонен в офлайн-режиме:', e.request.url);
-      });
-    })
+    fetch(e.request)
+      .then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200) {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(e.request, responseClone);
+          });
+        }
+        return networkResponse;
+      })
+      .catch(() => {
+        return caches.match(e.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+          console.log('Запрос отклонен в офлайн-режиме:', e.request.url);
+        });
+      })
   );
 });
